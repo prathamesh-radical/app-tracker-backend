f7c8d263aa013cae1a47ae618dce3f9e93018939
prathamesh-radical <prathmesh.n@radicalglobal.net>
2026-03-10 17:40:32 +0530

    route encrypted

--- routes/GetdataRoute.js
+++ routes/GetdataRoute.js
@@ -2,16 +2,17 @@ import express from "express";
 import {
     Debtors, GuestEntry, GuestEntryUsersData, Mechanic, MechanicServicesData, MechanicUsersData, Moneycollect, MoneycollectUsersData
 } from "../controller/GetDataController.js";
+import authenticateToken from "../middleware/verifyToken.js";
 
 const GetRoutes = express.Router();
 
-GetRoutes.get("/debtors", Debtors);
-GetRoutes.get("/mechanic", Mechanic);
-GetRoutes.get("/mechanic-data", MechanicUsersData);
-GetRoutes.get("/mechanic-services-data", MechanicServicesData);
-GetRoutes.get("/money-collect", Moneycollect);
-GetRoutes.get("/money-collect-data", MoneycollectUsersData);
-GetRoutes.get("/guest-entries", GuestEntry);
-GetRoutes.get("/guest-entries-data", GuestEntryUsersData);
+GetRoutes.get("/debtors", authenticateToken, Debtors);
+GetRoutes.get("/mechanic", authenticateToken, Mechanic);
+GetRoutes.get("/mechanic-data", authenticateToken, MechanicUsersData);
+GetRoutes.get("/mechanic-services-data", authenticateToken, MechanicServicesData);
+GetRoutes.get("/money-collect", authenticateToken, Moneycollect);
+GetRoutes.get("/money-collect-data", authenticateToken, MoneycollectUsersData);
+GetRoutes.get("/guest-entries", authenticateToken, GuestEntry);
+GetRoutes.get("/guest-entries-data", authenticateToken, GuestEntryUsersData);
 
 export default GetRoutes;
