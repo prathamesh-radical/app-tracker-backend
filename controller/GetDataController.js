854fae717e4350e60d785433e04b4e5218c71690
prathamesh-radical <prathmesh.n@radicalglobal.net>
2026-03-10 12:48:12 +0530

    bug fixed

--- controller/GetDataController.js
+++ controller/GetDataController.js
@@ -32,7 +32,20 @@ export const MechanicUsersData = async (req, res) => {
             if (err) {
                 return res.status(500).json({ message: "Error fetching values", success: false });
             }
-            res.status(200).json({ message: "mechanics users data fetched successfully", success: true, mechaniclist: result });
+            res.status(200).json({ message: "Mechanics users data fetched successfully", success: true, mechaniclist: result });
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", success: false });
+    }
+};
+
+export const MechanicServicesData = async (req, res) => {
+    try {
+        mechDB.query("SELECT * FROM services", (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: "Error fetching values", success: false });
+            }
+            res.status(200).json({ message: "Mechanics services data fetched successfully", success: true, serviceslist: result });
         });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", success: false });
--- routes/GetdataRoute.js
+++ routes/GetdataRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-    Debtors, GuestEntry, GuestEntryUsersData, Mechanic, MechanicUsersData, Moneycollect, MoneycollectUsersData
+    Debtors, GuestEntry, GuestEntryUsersData, Mechanic, MechanicServicesData, MechanicUsersData, Moneycollect, MoneycollectUsersData
 } from "../controller/GetDataController.js";
 
 const GetRoutes = express.Router();
@@ -8,6 +8,7 @@ const GetRoutes = express.Router();
 GetRoutes.get("/debtors", Debtors);
 GetRoutes.get("/mechanic", Mechanic);
 GetRoutes.get("/mechanic-data", MechanicUsersData);
+GetRoutes.get("/mechanic-services-data", MechanicServicesData);
 GetRoutes.get("/money-collect", Moneycollect);
 GetRoutes.get("/money-collect-data", MoneycollectUsersData);
 GetRoutes.get("/guest-entries", GuestEntry);
