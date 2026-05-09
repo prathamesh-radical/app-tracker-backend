44aa49ff1f408b2a2d659f832e369def46e471c7
prathamesh-radical <prathmesh.n@radicalglobal.net>
2026-05-09 12:41:05 +0530

    new function created

--- controller/BuddyWalkDataController.js
+++ controller/BuddyWalkDataController.js
@@ -1,12 +1,12 @@
 import { buddyWalkDB } from "../db/db.js";
 
-export const BuddyWalkStepsData = async (req, res) => {
+export const GetUsersData = async (req, res) => {
     try {
         buddyWalkDB.query("SELECT * FROM users", (err, result) => {
             if (err) {
                 return res.status(500).json({ message: "Error fetching values", success: false });
             }
-            res.status(200).json({ message: "BuddyWalk steps data fetched successfully", success: true, stepslist: result });
+            res.status(200).json({ message: "BuddyWalk user data fetched successfully", success: true, stepslist: result });
         });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", success: false });
@@ -33,6 +33,19 @@ export const GetGroupMemberData = async (req, res) => {
                 return res.status(500).json({ message: "Error fetching values", success: false });
             }
             res.status(200).json({ message: "BuddyWalk group member data fetched successfully", success: true, groupmemberlist: result });
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", success: false });
+    }
+};
+
+export const GetUsersStepsData = async (req, res) => {
+    try {
+        buddyWalkDB.query("SELECT * FROM steps", (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: "Error fetching values", success: false });
+            }
+            res.status(200).json({ message: "BuddyWalk steps data fetched successfully", success: true, steps: result });
         });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", success: false });
