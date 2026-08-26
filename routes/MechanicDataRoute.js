96e113c18a09d02a91427ae664d52a6ab1e0b631
Prathamesh Nimje <prathmesh.n@radicalglobal.net>
2026-08-26 12:04:57 +0530

    controller updated

--- routes/MechanicDataRoute.js
+++ routes/MechanicDataRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticateToken from "../middleware/verifyToken.js";
 import {
-    GetActiveUsersCount, GetAllSubscriptions, Mechanic, MechanicServicesData, MechanicSubscriptionData, MechanicUsersData, GetAllSubscriptionsByOrderIds
+    GetActiveUsersCount, Mechanic, MechanicServicesData, MechanicSubscriptionData, MechanicUsersData
 } from "../controller/MechanicDataController.js";
 
 const GetMechanicRoute = express.Router();
@@ -11,7 +11,7 @@ GetMechanicRoute.get("/mechanic-data", authenticateToken, MechanicUsersData);
 GetMechanicRoute.get("/mechanic-services-data", authenticateToken, MechanicServicesData);
 GetMechanicRoute.get("/mechanic-active-users", authenticateToken, GetActiveUsersCount);
 GetMechanicRoute.get("/mechanic-premium-users", authenticateToken, MechanicSubscriptionData);
-GetMechanicRoute.get("/all-subscriptions", authenticateToken, GetAllSubscriptions);
-GetMechanicRoute.post("/bulk-subscriptions", authenticateToken, GetAllSubscriptionsByOrderIds);
+// GetMechanicRoute.get("/all-subscriptions", authenticateToken, GetAllSubscriptions);
+// GetMechanicRoute.post("/bulk-subscriptions", authenticateToken, GetAllSubscriptionsByOrderIds);
 
 export default GetMechanicRoute;
